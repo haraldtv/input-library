4a1dbd676fddd2e830cdadfbab9eb07628da19ee
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-08-12 13:11:36 +0200

    Update input.c

--- input.c
+++ input.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //Function for getting int input. Simple, but not very configurable
-//End of function can be optimized a bit, but stays for readability
+//
+//Returns:  positive int - int entered by user
+//          -1 - Incorrect input
 int getInt() {
-    char *inp;
-    int retInt, len, i;
+    char inp[140];
+    int len, i;
 
-    if (fgets(inp, 130, stdin) == NULL) {
-        return 1;
+    if (fgets(inp, 128, stdin) == NULL) {
+        return -1;
     }
 
     len = strlen(inp); 
-    
-    for(i=0;i<len;i++) {
-        int isNum = 0;
+    int isNum = 0;
+    //(len-2) as that is the last character before the '\n' in the input string
+    for(i=0;i<len-1;i++) {
         if (!( (inp[i] < 58) && (inp[i] > 47) )){
             isNum = 1; 
         }
@@ -24,38 +27,49 @@ int getInt() {
         return (atoi(inp)); 
     }
     else {
-        return 1;
+        return -1;
     }
 
 }
-
+    //Function: getLine
+    //          Gets string input from user. Assumes valid ascii input
+    //Parameters: *inp - char array to be filled; size - size of the char array
+    //
+    //Returns:  0 - sucsess
+    //          1 - no input
+    //          2 - input too long (still puts the input read into the char array)
 int getLine(char *inp, int size) {
     char c;
     int of;
+    int returnVal = 0;
 
     //If the user doesn't specify a size, the program will free the input string and allocate a char array size 50
     //This is done to prevent the getLine function from creating an overflow bug
-    if (size == NULL) {
+
+    if (size == 0) {
         size = 48;
-        free(inp);
-        inp = malloc(sizeof(char)*50);
+        inp = (char*)realloc(inp, sizeof(char)*50);
     }
 
    if (fgets(inp, size, stdin) == NULL) {
         return 1;
     }
+    printf("%c\n", *(inp + (strlen(inp) - 1)) );
     //Flushes the input if the last character is not line break
-    if (inp[strlen(inp)-1] != "\n"){
-        while ( ((c = getchar()) != "\n") && (c != EOF) );
+    if (*(inp + (strlen(inp) - 1)) != '\n'){
+        while ( ((c = getchar()) != '\n') && (c != EOF) );
+        returnVal = 2;
     }
     //Repleace linebrake with \0 so it is formatted as a proper string
-    inp[strlen(inp)-1] = "\0";
-    return 0;
+    inp[strlen(inp)-1] = '\0';
+    return returnVal;
 }
 
 
-//Used to clear the std buffer 
+//Used to clear the stdin buffer 
+//Want to set a limit for the while loop, but don't know how large to set the max runs
 void clearBuffer() {
     char c;
-    while ( (c = (getchar()) != '\n') && (c != '\0') (c != EOF) );
+    int maxIterations = 10;
+    while ( (c = (getchar()) != '\n') && (c != '\0') && (c != EOF) );
 }
